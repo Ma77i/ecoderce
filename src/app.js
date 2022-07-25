8305ce05424de2170e394b43e7170cead5b672fe
mattiass <mattiasuru@gmail.com>
2022-07-25 18:35:39 -0300

    try 2

--- src/app.js
+++ src/app.js
@@ -80,10 +80,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/static", express.static(path.join(__dirname, "../public")));
 
-app.use(express.static(path.join(__dirname, "../client", "build")))
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(compression());
+  app.use(express.static(path.join(__dirname, "../client", "build")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 app.use(flash());
 app.use(cookieParser("This is a secret"));
